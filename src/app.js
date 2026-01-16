f20d2b1862c76879e71f8ee052928b2d6d39e2f9
Anishprajapat123 <anishprajapat924@gmail.com>
2026-01-16 20:55:59 +0530

    playing with routes and http methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
 
-app.use("/text",function(req,res){
+app.get("/user/:userId",(req,res)=>{
+    console.log(req.params);
+    res.send({firstname : "anish1", lastname: "prajapat"});
+})
+
+app.get("/user",(req,res)=>{
+    res.send({firstname: "anish" , lastname : "prajapat"});
+});
+
+app.post("/user",(req,res)=>{
+    console.log("save the data to the database");
+    res.send("Data successfully saved to database");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully");
+})
+
+
+app.use("/test",function(req,res){
     res.send("hello server from 3000");
 })
+
+
 
 app.listen(3000,()=>{
     console.log("server is started successfully");
