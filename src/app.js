4c119df0056a808c9a1c645ef1c87c8812e90ef5
Anishprajapat123 <anishprajapat924@gmail.com>
2026-01-17 16:01:30 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,31 +1,23 @@
-const express = require('express');
+const express = require("express")
+
 const app = express();
 
-app.get("/user/:userId",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname : "anish1", lastname: "prajapat"});
-})
+app.get("/user",(req,res,next)=>{
+    console.log("route handler 1");
+    next();
+}, 
+(req,res,next)=>{
+    console.log("route handler 2");
+    //res.send("handled by 2");
+    next();
+},
+(req,res)=>{
+    console.log("route handler 3");
+    res.send("handled by 3");
+}
 
-app.get("/user",(req,res)=>{
-    res.send({firstname: "anish" , lastname : "prajapat"});
-});
-
-app.post("/user",(req,res)=>{
-    console.log("save the data to the database");
-    res.send("Data successfully saved to database");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("deleted successfully");
-})
-
-
-app.use("/test",function(req,res){
-    res.send("hello server from 3000");
-})
-
-
+)
 
 app.listen(3000,()=>{
-    console.log("server is started successfully");
-});
+    console.log("server successfulyy");
+})
