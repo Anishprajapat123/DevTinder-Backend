e0fe680c32503d79569313952d2b2b4af7476344
Anish prajapat <91956695+Anishprajapat123@users.noreply.github.com>
2026-04-02 11:23:48 +0530

    Update database connection string
    Removed sensitive information from database connection string.

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose")
 const connectDB = async ()=>{
 
 mongoose.connect(
-    "mongodb+srv://anishprajapat924:NDV0Lk67o9brreUg@cluster0.pqidz.mongodb.net/devtinder"
 );
 };
 
